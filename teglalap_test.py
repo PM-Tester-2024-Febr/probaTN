934e5d5300c5f4b512ab60dd670b114f9d5390d3
topfernorbert <norbert.topfer@gmail.com>
2024-07-17 20:24:18 +0200

    próba

--- teglalap_test.py
+++ teglalap_test.py
@@ -1,49 +1,68 @@
+"""
+1. Feladat: Keressük a téglalap kerületét
+
+Készíts egy python applikációt (egy darab python file) ami selenium-ot használ.
+Az ellenőrzésekhez pytest keretrendszert használj, valamint fontos az `assert` összehasonlítások használata is!
+
+A program töltse be a téglalap kerülete app-ot. https://high-flyer.hu/hetihazi/feladat2_email.html
+
+Feladatod, hogy automatizáld selenium webdriverrel az alábbi funkcionalitásokat a téglalap kerülete appban:
+
+* Helyes kitöltés esete:
+    * a: 74
+    * b: 32
+    * Eredmény: 212
+
+* Nem számokkal történő kitöltés:
+    * a: kiskutya
+    * b: 32
+    * Eredmény: NaN
+
+* Üres kitöltés:
+    * a: <üres>
+    * b: <üres>
+    * Eredmény: NaN
+"""
+
+import os
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 
 
-class TestRectangle(object):
+class TestRectangle():
+    PATH = "https://high-flyer.hu/hetihazi/feladat1_teglalap.html"
+
     def setup_method(self):
-        options = Options()
+        options = ChromeOptions()
         options.add_experimental_option('detach', True)
+        self.browser.set_window_size(1024, 800)
         self.browser = webdriver.Chrome(options=options)
-        options.add_argument('--headless')
-        self.browser.set_window_size(1024,800)
-        self.browser.get('https://high-flyer.hu/hetihazi/feladat1_teglalap.html')
+        self.browser.get(TestRectangle.PATH)
+        self.browser.maximize_window()
 
     def teardown_method(self):
         self.browser.quit()
 
+    def test_fill_valid(self):
+        input_a = self.browser.find_element(By.ID, 'a')
+        input_a.send_keys('74')
+        input_b = self.browser.find_element(By.ID, 'b')
+        input_b.send_keys('32')
+        button_calc = self.browser.find_element(By.ID, 'submit')
+        button_calc.click()
+        assert self.browser.find_element(By.ID, 'result').text == '212'
 
-    def test_rectangle_positive(self):
-        a_side = self.browser.find_element(By.ID,'a')
-        a_side.send_keys('74')
-        b_side = self.browser.find_element(By.ID,'b')
-        b_side.send_keys('32')
-        calculate = self.browser.find_element(By.ID,'submit')
-        calculate.click()
-        result = self.browser.find_element(By.ID,'result')
-        assert result.text == '212'
+    def test_fill_invalid(self):
+        input_a = self.browser.find_element(By.ID, 'a')
+        input_a.send_keys('kiskutya')
+        input_b = self.browser.find_element(By.ID, 'b')
+        input_b.send_keys('32')
+        button_calc = self.browser.find_element(By.ID, 'submit')
+        button_calc.click()
+        assert self.browser.find_element(By.ID, 'result').text == 'NaN'
 
-    def test_rectangle_word(self):
-        a_side = self.browser.find_element(By.ID,'a')
-        a_side.send_keys('kiskutya')
-        b_side = self.browser.find_element(By.ID,'b')
-        b_side.send_keys('32')
-        calculate = self.browser.find_element(By.ID,'submit')
-        calculate.click()
-        result = self.browser.find_element(By.ID,'result')
-        assert result.text == 'NaN'
-
-    def test_rectangle_empty(self):
-        a_side = self.browser.find_element(By.ID, 'a')
-        a_side.send_keys('')
-        b_side = self.browser.find_element(By.ID, 'b')
-        b_side.send_keys('')
-        calculate = self.browser.find_element(By.ID, 'submit')
-        calculate.click()
-        result = self.browser.find_element(By.ID, 'result')
-        assert result.text == 'NaN'
-        
-
+    def test_no_fill(self):
+        button_calc = self.browser.find_element(By.ID, 'submit')
+        button_calc.click()
+        assert self.browser.find_element(By.ID, 'result').text == 'NaN'
